1b45bf5a42e25c2dfc632a95783dac6b0254c7a4
Filipescu Mihaela <mihaela-teodora.filipescu@s.unibuc.ro>
2026-03-21 13:11:43 +0200

    Conference schedule page

---
+++ Pages/Conference/Schedule.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConferenceSite.Pages.Conference
+{
+    public class ScheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
