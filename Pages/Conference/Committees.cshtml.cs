edc0072215ae626a9f3107caafe70b5960c242e1
Filipescu Mihaela <mihaela-teodora.filipescu@s.unibuc.ro>
2026-03-26 17:17:00 +0200

    Add committee page

---
+++ Pages/Conference/Committees.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConferenceSite.Pages.Conference
+{
+    public class CommitteeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
