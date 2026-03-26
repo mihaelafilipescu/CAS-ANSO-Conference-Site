a9ea632600049a88fea408928b8a677b525d3e17
Filipescu Mihaela <mihaela-teodora.filipescu@s.unibuc.ro>
2026-03-26 16:40:39 +0200

    Add ROMPHYSCHEM posters page

---
+++ Pages/Conference/Posters.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConferenceSite.Pages.Conference
+{
+    public class PostersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
